0ead40c3449186373aa2147a6acf3aef5d76788e
jsofiya1007-cmd <j.sofiya1007@gmail.com>
2026-04-18 17:00:59 +0530

    Create ExcelReader.java to read test data from Excel file using Apache POI

---
+++ src/main/java/utilities/ExcelReader.java
@@ -0,0 +1,52 @@
+package utilities;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ExcelReader {
+
+    private String excelFilePath;
+
+    public ExcelReader(String excelFilePath) {
+        this.excelFilePath = excelFilePath;
+    }
+
+    public void readExcel() {
+        try (FileInputStream fis = new FileInputStream(excelFilePath);
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    // Assuming you want to print the cell value
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        default:
+                            System.out.print("\t");
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
